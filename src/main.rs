16d783af05319d698690c46a22ca0a5077b54d4d
enuesaa <nsrciog@gmail.com>
2023-01-04 17:07:07 +0900

    feat: init
    - `cargo add wry`
    - add sample sode to main.rs

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use wry::{
+  application::{
+    dpi::{LogicalSize},
+    event::{Event, StartCause, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+  },
+  webview::WebViewBuilder,
+};
+
+fn main() -> wry::Result<()> {
+  let event_loop = EventLoop::new();
+  let window = WindowBuilder::new()
+    .with_inner_size(LogicalSize::new(700, 1000))
+    .build(&event_loop)?;
+  
+  let user_agent_string = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Mobile/15E148 Safari/604.1";
+  
+  let webview = WebViewBuilder::new(window)?
+    .with_user_agent(&user_agent_string)
+    .with_url("https://yahoo.co.jp")?
+    .build()?;
+  
+  let scale_factor = 1.9;
+  webview.zoom(scale_factor);
+
+  event_loop.run(move |event, _, control_flow| {
+    *control_flow = ControlFlow::Wait;
+
+    match event {
+      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+      Event::WindowEvent {
+        event: WindowEvent::CloseRequested,
+        ..
+      } => *control_flow = ControlFlow::Exit,
+      _ => (),
+    }
+  });
+}
