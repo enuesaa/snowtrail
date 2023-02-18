ff59ae833e8c3c029d4462e4db1e6d78f2d36b2e
enuesaa <nsrciog@gmail.com>
2023-02-18 19:38:19 +0900

    feat: add menu bar

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use wry::{
         dpi::LogicalSize,
         event::{Event, StartCause, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
+        menu::{MenuBar, MenuItem},
         window::WindowBuilder,
     },
     webview::WebViewBuilder,
@@ -10,8 +11,16 @@ use wry::{
 
 fn main() -> wry::Result<()> {
     let event_loop = EventLoop::new();
+
+    let mut menu_bar = MenuBar::new();
+    let mut utilities = MenuBar::new();
+    utilities.add_native_item(MenuItem::Quit);
+    utilities.add_native_item(MenuItem::Services);
+    menu_bar.add_submenu("Utilities", true, utilities);
+
     let window = WindowBuilder::new()
         .with_inner_size(LogicalSize::new(1500, 1000))
+        .with_menu(menu_bar)
         .build(&event_loop)?;
 
     let webview = WebViewBuilder::new(window)?
