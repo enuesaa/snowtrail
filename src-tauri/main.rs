9eb483c4bdfa757714ccb6703d864ce41cf2c1f8
enuesaa <nsrciog@gmail.com>
2023-02-23 18:47:33 +0900

    feat: fetch rss feed

--- src-tauri/main.rs
+++ src-tauri/main.rs
@@ -1,6 +1,20 @@
+use std::error::Error;
+use rss::Channel;
+use reqwest;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[allow(dead_code)]
+async fn example_feed() -> Result<Channel, Box<dyn Error>> {
+    let content = reqwest::get("https://www.yahoo.com/news/rss")
+        .await?
+        .bytes()
+        .await?;
+    let channel = Channel::read_from(&content[..])?;
+    Ok(channel)
 }
 
 fn main() {
