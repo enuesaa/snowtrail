283de0d53630516abf75f837724a2000487ed463
enuesaa <nsrciog@gmail.com>
2023-02-25 19:13:56 +0900

    refactor(frontend): use component

---
+++ src/components/feed/FeedBoard.tsx
@@ -0,0 +1,34 @@
+import { invoke } from '@tauri-apps/api/tauri'
+import { useRef, useState, MouseEventHandler } from 'react'
+import { FeedItem } from './FeedItem'
+
+type MessageItem = {
+  title: string;
+  url: string;
+}
+type Message = {
+  title: string;
+  items: MessageItem[];
+}
+export const FeedBoard = () => {
+  const [message, setMessage] = useState<Message>({ title: '', items: [] })
+  const urlInput = useRef<HTMLInputElement>(null)
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
+    e.preventDefault()
+    const url = urlInput.current?.value ?? ''
+    const res = await invoke('feed', { url })
+    setMessage(res as Message)
+  }
+
+  return (
+    <div style={{ color: '#fafafa' }}>
+      <input ref={urlInput} />
+      <button onClick={handleClick}>Greet</button>
+      <h2>{message.title}</h2>
+      {message.items.map((m,i) => (
+        <FeedItem key={i} url={m.url} title={m.title} />
+      ))}
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,11 @@
 import { Header } from '../components/common/Header'
-import { Main } from '../components/common/Main'
-import { invoke } from '@tauri-apps/api/tauri'
-import { useRef, useState, MouseEventHandler } from 'react'
-import { FeedItem } from '../components/feed/FeedItem'
+import { FeedBoard } from '../components/feed/FeedBoard'
 
-type MessageItem = {
-  title: string;
-  url: string;
-}
-type Message = {
-  title: string;
-  items: MessageItem[];
-}
 export default function TopPage() {
-  const [message, setMessage] = useState<Message>({ title: '', items: [] })
-  const urlInput = useRef<HTMLInputElement>(null)
-
-  const handleClick: MouseEventHandler<HTMLButtonElement> = async (e) => {
-    e.preventDefault()
-    const url = urlInput.current?.value ?? ''
-    const res = await invoke('feed', { url })
-    setMessage(res as Message)
-  }
-
   return (
     <>
       <Header />
-      <Main>
-        <div style={{ color: '#fafafa' }}>
-          <input ref={urlInput} placeholder="Enter a url..." />
-          <button onClick={handleClick}>Greet</button>
-          <h2>{message.title}</h2>
-          {message.items.map((m,i) => (
-            <FeedItem key={i} url={m.url} title={m.title} />
-          ))}
-        </div>
-      </Main>
+      <FeedBoard />
     </>
   )
 }
