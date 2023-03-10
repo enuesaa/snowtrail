66108c91436b9cc831816112326ae1e8020ec760
enuesaa <nsrciog@gmail.com>
2023-03-11 02:17:19 +0900

    feat: plugin-store でファイル保存できるようにした

--- src-tauri/main.rs
+++ src-tauri/main.rs
@@ -2,6 +2,9 @@ pub mod commands;
 
 use commands::greet::greet;
 use commands::feed::feed;
+use tauri_plugin_store::StoreBuilder;
+use serde_json::json;
+use std::env;
 
 #[cfg(target_os = "macos")]
 #[macro_use]
@@ -22,6 +25,15 @@ fn main() {
                     let () = msg_send![webview.inner(), setAllowsBackForwardNavigationGestures: true];
                 }
             })?;
+            Ok(())
+        })
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .setup(|app| {
+            let mut filename = env::current_dir()?;
+            filename.push("aa.json");
+            let mut store = StoreBuilder::new(app.handle(), filename).build();
+            store.insert("a".to_string(), json!("b"))?;
+            store.save()?;
             Ok(())
         })
         .run(tauri::generate_context!())
