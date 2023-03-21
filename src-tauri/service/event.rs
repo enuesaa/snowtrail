9044e5ee238c25a204c67a19bed5c92a0a1e11ed
enuesaa <nsrciog@gmail.com>
2023-03-21 23:59:12 +0900

    feat: save sata as json

--- src-tauri/service/event.rs
+++ src-tauri/service/event.rs
@@ -1,18 +1,26 @@
 use serde::{Serialize, Deserialize};
 use crate::repository::redis::Redis;
+use serde_json;
+
+#[derive(Serialize, Deserialize)]
+pub struct EventMeta {
+    name: String,
+    value: String,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct Event {
-    title: String,
-    marketing: bool,
+    name: String,
+    meta: Vec<EventMeta>,
 }
 impl Event {
     pub fn new(name: &str) -> Self {
-        Event { title: name.to_string(), marketing: false }
+        Event { name: name.to_string(), meta: vec![] }
     }
 
     pub fn create(self) {
         let redis = Redis::new();
-        let _ = redis.create(&self.title, "a");
+        let key = self.name.clone();
+        let _ = redis.create(&key, &serde_json::to_string(&self).unwrap());
     }
 }
