140a6ff7ae67c610ea321d46e155857555ae7524
enuesaa <nsrciog@gmail.com>
2023-03-30 23:56:53 +0900

    feat(redis): add function to save or get json data

--- src-tauri/repository/redis.rs
+++ src-tauri/repository/redis.rs
@@ -40,6 +40,39 @@ impl Redis {
         Ok(vec![])
     }
 
+    pub fn json_get(self, key: &str) -> Result<Vec<String>, Box<dyn Error>> {
+        let client = Client::open("redis://localhost:6380/")?;
+        let res = client.get_connection();
+        if let Err(b) = res {
+            println!("{:?}", b);
+        } else {
+            let mut con = res.unwrap();
+            let a = con.json_get::<String, String, Vec<String>>(key.to_string(), ".".to_string());
+            if let Err(a) = a {
+                println!("{:?}", a);
+            } else {
+                return Ok(a.unwrap());
+            }
+        }
+        Ok(vec![])
+    }
+    
+    pub fn json_set(self, key: &str, value: &str) -> Result<Vec<String>, Box<dyn Error>> {
+        let client = Client::open("redis://localhost:6380/")?;
+        let res = client.get_connection();
+        if let Err(b) = res {
+            println!("{:?}", b);
+        } else {
+            let mut con = res.unwrap();
+            let a = con.json_set::<String, String, String, Vec<String>>(key.to_string(), ".".to_string(), &value.to_string());
+            if let Err(a) = a {
+                println!("{:?}", a);
+            } else {
+                return Ok(a.unwrap());
+            }
+        }
+        Ok(vec![])
+    }
 }
 
 impl RepositoryTrait<()> for Redis {
