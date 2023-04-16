e272e196016ecb74d9c6baea35eb81ad4af44a16
enuesaa <nsrciog@gmail.com>
2023-04-16 14:22:41 +0900

    feat: command interface

--- src-tauri/command/script.rs
+++ src-tauri/command/script.rs
@@ -2,16 +2,38 @@ use serde::{Serialize, Deserialize};
 use crate::repository::runcommand::Runcommand;
 use dirs::home_dir;
 
+use super::project::ProjectSchema;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ScriptSchema {
     name: String,
     commands: Vec<String>, // createfile
-    project_id: String, // run command under this project
+    project_name: String, // run command under this project
 }
 
 #[tauri::command]
-pub fn create_script(req: ScriptSchema) {
-    println!("{:?}", req);
+pub fn script_list(project_name: String) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn script_get(name: String) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn script_create(data: ProjectSchema) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn script_delete(name: String) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn script_run(name: String) {
+    todo!()
 }
 
 #[tauri::command]
--- src-tauri/command/subscribe.rs
+++ src-tauri/command/subscribe.rs
@@ -3,12 +3,26 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SubscribeSchema {
     name: String,
-    rule: Vec<String>, // can subscribe snowtrail event
+    matched: String, // can subscribe event like 'snowtrail:log:{name}#publish' or 'app:event:{name}#publish'
     script_name: String,
 }
 
 #[tauri::command]
-pub fn create_subscribe(data: SubscribeSchema) {
-    println!("{:?}", data);
-    todo!();
+pub fn subscribe_list() {
+    todo!()
+}
+
+#[tauri::command]
+pub fn subscribe_get(name: String) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn subscribe_create(data: SubscribeSchema) {
+    todo!()
+}
+
+#[tauri::command]
+pub fn subscribe_delete(name: String) {
+    todo!()
 }
