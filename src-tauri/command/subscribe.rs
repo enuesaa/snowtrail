11a97ccb1605582e07046282f23cc6a739305c6d
enuesaa <nsrciog@gmail.com>
2023-04-29 17:55:17 +0900

    feat: subscribe service

--- src-tauri/command/subscribe.rs
+++ src-tauri/command/subscribe.rs
@@ -1,4 +1,6 @@
 use serde::{Serialize, Deserialize};
+use crate::repository::rocks::RocksRepository;
+use crate::service::event::{Event, EventService, Subscribe};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SubscribeSchema {
@@ -8,21 +10,28 @@ pub struct SubscribeSchema {
 }
 
 #[tauri::command]
-pub fn subscribe_list() {
-    todo!()
+pub fn subscribe_list() -> Vec<SubscribeSchema> {
+    let subscribes = EventService::list_subscribes(RocksRepository {});
+    let mut ret = vec![];
+    for subscribe in subscribes {
+        ret.push(SubscribeSchema { name: subscribe.name, matched: "".to_string(), script_name: "".to_string() });
+    };
+    ret
 }
 
 #[tauri::command]
-pub fn subscribe_get(name: String) {
-    todo!()
+pub fn subscribe_get(id: String) -> SubscribeSchema {
+    let subscribe = EventService::get_subscribe(RocksRepository {}, &id);
+    SubscribeSchema { name: subscribe.name, matched: "".to_string(), script_name: "".to_string() }
 }
 
 #[tauri::command]
 pub fn subscribe_create(data: SubscribeSchema) {
-    todo!()
+    let subscribe: Subscribe = Subscribe::new(&data.name);
+    EventService::create_subscribe(RocksRepository {}, subscribe);
 }
 
 #[tauri::command]
-pub fn subscribe_delete(name: String) {
-    todo!()
+pub fn subscribe_delete(id: String) {
+    EventService::delete_subscribe(RocksRepository {}, &id);
 }
--- src-tauri/service/event.rs
+++ src-tauri/service/event.rs
@@ -5,11 +5,12 @@ use uuid::Uuid;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Subscribe {
-    name: String
+    pub id: Option<String>,
+    pub name: String
 }
 impl Subscribe {
     pub fn new(name: &str) -> Self {
-        Subscribe { name: name.to_string() }
+        Subscribe { id: None, name: name.to_string() }
     }
 }
 
@@ -82,19 +83,32 @@ impl EventService {
         rocks.delete("event", id);
     }
 
-    pub fn list_subscribes() -> Vec<Subscribe> {
-        todo!()
+    pub fn list_subscribes(rocks: RocksRepository) -> Vec<Subscribe> {
+        let kvs = rocks.list("subscribe", "", 100);
+        println!("{:?}", kvs);
+        let mut list: Vec<Subscribe> = vec![];
+        for kv in kvs {
+            let mut subscribe: Subscribe = serde_json::from_str(&kv.value).unwrap();
+            subscribe.id = Some(kv.key);
+            list.push(subscribe);
+        };
+        list
     }
 
-    pub fn get_subscribe(id: &str) -> Subscribe {
-        todo!()
+    pub fn get_subscribe(rocks: RocksRepository, id: &str) -> Subscribe {
+        let res = rocks.get("subscribe", id);
+        let mut subscribe: Subscribe = serde_json::from_str(&res.value).unwrap();
+        subscribe.id = Some(res.key);
+        subscribe
     }
 
-    pub fn create_subscribe(subscribe: Subscribe) -> String {
-        todo!()
+    pub fn create_subscribe(rocks: RocksRepository, subscribe: Subscribe) -> String {
+        let id = Uuid::new_v4().to_string();
+        rocks.put("subscribe", &id, &serde_json::to_string(&subscribe).unwrap());
+        id
     }
 
-    pub fn delete_subscribe(id: &str) {
-        todo!()
+    pub fn delete_subscribe(rocks: RocksRepository, id: &str) {
+        rocks.delete("subscribe", id);
     }
 }
